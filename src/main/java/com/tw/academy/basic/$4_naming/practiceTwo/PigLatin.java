7c2ee503e4f9178b5c8c49fef580a3a218df8bdb
LIKA8 <karen.c.w.li@cargosmart.com>
2021-06-22 11:53:15 +0800

    rename

--- src/main/java/com/tw/academy/basic/$4_naming/practiceTwo/PigLatin.java
+++ src/main/java/com/tw/academy/basic/$4_naming/practiceTwo/PigLatin.java
@@ -2,25 +2,27 @@ package com.tw.academy.basic.$4_naming.practiceTwo;
 
 public class PigLatin {
     //Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
-    public static String pigIt(String str) {
-        String[] arr_mark={".",",","-",":",";","!","?"};
-        String[] arr = str.split(" ");
-        char c;
+    public static String pigIt(String sentence) {
+        String[] punctuations = {".",",","-",":",";","!","?"};
+        String[] words = sentence.split(" ");
+        char firstWord;
         boolean flag = true;
 
-        for (int i = 0; i < arr.length; i++){
-            for (int j = 0; j < arr_mark.length; j++)
-                if(arr[i].contains(arr_mark[j]) == true)
+        for (int i = 0; i < words.length; i++){
+            for (String punctuation : punctuations){
+                if (words[i].contains(punctuation)) {
                     flag = false;
-
+                    break;
+                }
+            }
             if (flag){
-                c = arr[i].charAt(0);
-                arr[i] = arr[i].substring(1, arr[i].length());
-                arr[i] = arr[i].replace(arr[i], arr[i] + c + "ay");
+                firstWord = words[i].charAt(0);
+                words[i] = words[i].substring(1);
+                words[i] = words[i].replace(words[i], words[i] + firstWord + "ay");
             }
             flag = true;
         }
 
-        return String.join(" ", arr);
+        return String.join(" ", words);
     }
 }
