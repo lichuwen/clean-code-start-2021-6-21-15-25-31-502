fca0ffb0f3b65189675dff269c76ff1a9b70542a
LIKA8 <karen.c.w.li@cargosmart.com>
2021-06-22 11:52:25 +0800

    extra magic number

--- src/main/java/com/tw/academy/basic/$2_magic_number/FareCalculation.java
+++ src/main/java/com/tw/academy/basic/$2_magic_number/FareCalculation.java
@@ -2,11 +2,15 @@ package com.tw.academy.basic.$2_magic_number;
 
 public class FareCalculation {
 
+    public static final int MIDDLE_BOUNDARY_OF_DISCOUNT = 150;
+    public static final int MIN_BOUNDARY_OF_8_DISCOUNT = 100;
+    public static final int MAX_BOUNDARY_OF_5_DISCOUNT = 400;
+
     public double calculate(double originalPrice, double cumulativeExpensesThisMonth) {
-        if (cumulativeExpensesThisMonth >= 100 && cumulativeExpensesThisMonth < 150) {
+        if (cumulativeExpensesThisMonth >= MIN_BOUNDARY_OF_8_DISCOUNT && cumulativeExpensesThisMonth < MIDDLE_BOUNDARY_OF_DISCOUNT) {
             return originalPrice * 0.8;
         }
-        if (cumulativeExpensesThisMonth >= 150 && cumulativeExpensesThisMonth < 400) {
+        if (cumulativeExpensesThisMonth >= MIDDLE_BOUNDARY_OF_DISCOUNT && cumulativeExpensesThisMonth < MAX_BOUNDARY_OF_5_DISCOUNT) {
             return originalPrice * 0.5;
         }
         return originalPrice;
